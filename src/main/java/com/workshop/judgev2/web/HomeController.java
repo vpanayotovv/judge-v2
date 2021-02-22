95e6ad39a1fb7917e8dbc0bf22ca5f2123e7bcda
Vasil <vvpanayotovv@gmail.com>
2021-02-22 22:08:03 +0200

    implement home page functionality

--- src/main/java/com/workshop/judgev2/web/HomeController.java
+++ src/main/java/com/workshop/judgev2/web/HomeController.java
@@ -1,21 +1,30 @@
 package com.workshop.judgev2.web;
 
 import com.workshop.judgev2.security.CurrentUser;
+import com.workshop.judgev2.service.ExerciseService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    private final ExerciseService exerciseService;
     private final CurrentUser currentUser;
 
-    public HomeController(CurrentUser currentUser) {
+    public HomeController(ExerciseService exerciseService, CurrentUser currentUser) {
+        this.exerciseService = exerciseService;
         this.currentUser = currentUser;
     }
 
     @GetMapping("/")
-    public String index(){
-        return currentUser.isAnonymous() ? "index" : "home";
+    public String index(Model model){
+        if (currentUser.isAnonymous()){
+            return "index";
+        }
+
+        model.addAttribute("exercises",exerciseService.getAllExercise());
+        return "home";
     }
 
 }
